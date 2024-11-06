fa291a0f6edc2ece9fa898eb2882928843a40f58
ale-aguiar <127619012+ale-aguiar@users.noreply.github.com>
2024-11-06 16:33:55 +0000

    Paleta de colores / Página de inicio empezada
    Creada una paleta de colores especial para el proyecto en tailwind.config.js.
    Página de inicio empezada, header y main prácticamente terminados.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'finur-50': '#f3faeb',
+        'finur-100': '#e4f3d4',
+        'finur-200': '#cbe8ae',
+        'finur-300': '#a8d87e',
+        'finur-400': '#89c655',
+        'finur-500': '#6eb139',
+        'finur-600': '#508828',
+        'finur-700': '#3f6823',
+        'finur-800': '#355321',
+        'finur-900': '#2e481f',
+        'finur-950': '#16270c',
+      },
+    },
   },
   plugins: [],
 }
-
